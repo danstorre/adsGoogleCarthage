3bb58f5d24045382301f96407552c9464a4434ec
Daniel Torres <dtorres@eudaimonia.com.ar>
2018-02-08 10:24:56 -0300

    added framework

---
+++ adstarget/adstarget.h
@@ -0,0 +1,19 @@
+//
+//  adstarget.h
+//  adstarget
+//
+//  Created by Daniel Torres on 2/8/18.
+//  Copyright © 2018 Wallmart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for adstarget.
+FOUNDATION_EXPORT double adstargetVersionNumber;
+
+//! Project version string for adstarget.
+FOUNDATION_EXPORT const unsigned char adstargetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <adstarget/PublicHeader.h>
+
+
